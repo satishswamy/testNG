b1340cf32e5a1eca469f6f0aa1ab62b731d9e33b
Satish S Swamynathan <satishsswamy@hotmail.com>
2018-06-19 16:36:04 -0400

    commit2

--- Maven_Kafka/src/main/java/com/mavenKafka/SelDemo.java
+++ Maven_Kafka/src/main/java/com/mavenKafka/SelDemo.java
@@ -3,6 +3,6 @@ import org.testng.annotations.Test;
 public class SelDemo {
 	@Test
 	public void PrintSel() {
-		System.out.println("This is selenium demo");
+		System.out.println("This is selenium demo 2");
 	}
 }
